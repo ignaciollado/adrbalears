2da7d0b46004d01c0027dbe63a68527ff30a5b8a
ignaciollado <86431188+ignaciollado@users.noreply.github.com>
2024-04-17 07:14:01 +0200

    he actualizado el body

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -20,46 +20,82 @@ export class HeaderComponent {
       },
   
       language: {
-          default: 'en',
+          default: 'cas',
           translations: {
-              en: {
-                  consentModal: {
-                      title: 'Usamos cookies',
-                      description: 'Cookie modal description',
-                      acceptAllBtn: 'Aceptar todo',
-                      acceptNecessaryBtn: 'Rechazar todo',
-                      showPreferencesBtn: 'Manage Individual preferences'
-                  },
-                  preferencesModal: {
-                      title: 'Gestionar cookies',
-                      acceptAllBtn: 'Aceptar todo',
-                      acceptNecessaryBtn: 'Rechazar todo',
-                      savePreferencesBtn: 'Aceptar selección actual',
-                      closeIconLabel: 'Cerrar modal',
-                      sections: [
-                          {
-                              title: 'Somebody said ... cookies?',
-                              description: 'I want one!'
-                          },
-                          {
-                              title: 'Strictly Necessary cookies',
-                              description: 'These cookies are essential for the proper functioning of the website and cannot be disabled.',
-  
-                              //this field will generate a toggle linked to the 'necessary' category
-                              linkedCategory: 'necessary'
-                          },
-                          {
-                              title: 'Performance and Analytics',
-                              description: 'These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.',
-                              linkedCategory: 'analytics'
-                          },
-                          {
-                              title: 'More information',
-                              description: 'For any queries in relation to my policy on cookies and your choices, please <a href="#contact-page">contact us</a>'
-                          }
-                      ]
-                  }
-              }
+            en: {
+                consentModal: {
+                    title: 'We use cookies',
+                    description: 'Cookie modal description',
+                    acceptAllBtn: 'Accept all',
+                    acceptNecessaryBtn: 'Reject all',
+                    showPreferencesBtn: 'Manage Individual preferences'
+                },
+                preferencesModal: {
+                    title: 'Manage cookie preferences',
+                    acceptAllBtn: 'Accept all',
+                    acceptNecessaryBtn: 'Reject all',
+                    savePreferencesBtn: 'Accept current selection',
+                    closeIconLabel: 'Close modal',
+                    sections: [
+                        {
+                            title: 'Somebody said ... cookies?',
+                            description: 'I want one!'
+                        },
+                        {
+                            title: 'Strictly Necessary cookies',
+                            description: 'These cookies are essential for the proper functioning of the website and cannot be disabled.',
+                            //this field will generate a toggle linked to the 'necessary' category
+                            linkedCategory: 'necessary'
+                        },
+                        {
+                            title: 'Performance and Analytics',
+                            description: 'These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.',
+                            linkedCategory: 'analytics'
+                        },
+                        {
+                            title: 'More information',
+                            description: 'For any queries in relation to my policy on cookies and your choices, please <a href="#contact-page">contact us</a>'
+                        }
+                    ]
+                }
+            },
+            cas: {
+                consentModal: {
+                    title: 'Usamos cookies',
+                    description: 'Descripción de las cookies',
+                    acceptAllBtn: 'Aceptar todo',
+                    acceptNecessaryBtn: 'Rechazar todo',
+                    showPreferencesBtn: 'Manage Individual preferences'
+                },
+                preferencesModal: {
+                    title: 'Gestionar cookies',
+                    acceptAllBtn: 'Aceptar todo',
+                    acceptNecessaryBtn: 'Rechazar todo',
+                    savePreferencesBtn: 'Aceptar selección actual',
+                    closeIconLabel: 'Cerrar modal',
+                    sections: [
+                        {
+                            title: '¿Qué son las cookies?',
+                            description: 'son archivos de texto con pequeños datos, como un nombre de usuario y contraseña, que se utilizan para identificar tu ordenador cuando utilizas una red. Se utilizan cookies específicas para identificar a usuarios concretos y mejorar su experiencia de navegación por la web. Los datos almacenados en una cookie son creados por el servidor al conectarte. Estos datos se etiquetan con un ID exclusivo para ti y tu ordenador. Cuando la cookie se intercambia entre un ordenador y el servidor de la red, este último lee el ID y sabe qué información específica mostrarte.'
+                        },
+                        {
+                            title: 'Strictly Necessary cookies',
+                            description: 'These cookies are essential for the proper functioning of the website and cannot be disabled.',
+                            //this field will generate a toggle linked to the 'necessary' category
+                            linkedCategory: 'necessary'
+                        },
+                        {
+                            title: 'Performance and Analytics',
+                            description: 'These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.',
+                            linkedCategory: 'analytics'
+                        },
+                        {
+                            title: 'Más información',
+                            description: 'For any queries in relation to my policy on cookies and your choices, please <a href="#contact-page">contact us</a>'
+                        }
+                    ]
+                }
+            }
           }
       }
   });
