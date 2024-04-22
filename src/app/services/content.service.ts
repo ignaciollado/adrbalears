0f32cddadc7066da95d3b5ae4bda16880c21f816
ignaciollado <86431188+ignaciollado@users.noreply.github.com>
2024-04-22 13:57:14 +0200

    estoy con el diseño la APIREST para la gestión de los contenidos

--- src/app/services/content.service.ts
+++ src/app/services/content.service.ts
@@ -8,7 +8,14 @@ import { Observable } from 'rxjs';
 })
 export class ContentService {
   private url = 'http://jsonplaceholder.typicode.com/posts';
-
+  /* key para JOOMLA 4 */
+  private jToken = 'c2hhMjU2OjkwNzpjZjRkMmYwOTkxMGYyZTFiMWY2NGFjYThjZWVjM2VlNmI4ZGRlNGU1OTBjNzNiODA0NzM2NDdhYjUwN2M4NTdm'
+  private apiBaseUrlOne = 'https://contents.idi.es/api/index.php/v1/content/articles'
+  private apiBaseUrl = 'https://contents.idi.es/api/index.php/v1/content/articles?page[offset]=0&page[limit]=200' /* Chapuza para mostrar todo ya que no veo como pasar query parameters */
+  private apiBaseUrlEverything = 'https://contents.idi.es/api/index.php/v1/content/articles?page[offset]=0&page[limit]=1800' /* Chapuza para mostrar todo ya que no veo como pasar query parameters */
+  private apiBaseUrlLastContent = 'https://contents.idi.es/api/index.php/v1/content/articles?page[offset]=0&page[limit]=20' /* Offset 0 y tamaño de página a 20 artículos */
+  /*  */
+  
   constructor(private httpClient: HttpClient) { }
 
   getAll(): Observable<genericDataDTO[]> {
