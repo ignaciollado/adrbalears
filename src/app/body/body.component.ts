a1b2ae7362975ae32479d79b441002299791a3a7
ignaciollado <86431188+ignaciollado@users.noreply.github.com>
2024-04-29 09:51:15 +0200

    he modificado app component y body

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+/* import { FormBuilder, FormGroup, Validators } from '@angular/forms'; */
 import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource, NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
 import { ArticleContentService } from '../services/article-content.service';
 import { genericDataDTO } from '../model/generic-data.model';
@@ -15,45 +15,46 @@ import { TranslateService } from '@ngx-translate/core';
 
 export class BodyComponent {
 
-	searchTheWebForm!: FormGroup
+	/* searchTheWebForm!: FormGroup */
 	totalFound: string = ""
+	showSearch: boolean = true
 	public contenidos: reqArticle[] = []
 	currentLang: string = ""
 	isCollapsed:boolean = true
   images = [62, 83, 466, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/868/500`)
 
-	paused = false
-	unpauseOnArrow = false
-	pauseOnIndicator = false
-	pauseOnHover = true
-	pauseOnFocus = true
+	paused:boolean = false
+	unpauseOnArrow:boolean = false
+	pauseOnIndicator:boolean = false
+	pauseOnHover:boolean = true
+	pauseOnFocus:boolean = true
 	genericDataContents: genericDataDTO[] = []
-	theLeftLema: string = "Sección variable (según la <strong>PREGUNTA 3</strong>)"
+	theLeftLema: string = "Sección variable (según la <strong>PREGUNTA 03</strong>)"
 	theCenterLema: string = "<h1>Llamadas a la acción</h1><p>Mensaje que motive al usuario a conectar a través del formulario más apropiado y botón que redirija al mismo</p>"
 	theRightLema: string = "Solicitar Asesoramiento<br>para mi StartUp"
 
   constructor(config: NgbTooltipConfig,
 		private contentService: ArticleContentService,
-		private formBuilder: FormBuilder,
+		/* private formBuilder: FormBuilder, */
 		public translateService: TranslateService, 
 		private searchService: SearchTheWebService) {
-				// customize default values of tooltips used by this component tree
-				config.placement = 'bottom';
-				config.triggers = 'hover';
-				}
+			// customize default values of tooltips used by this component tree
+			config.placement = 'bottom';
+			config.triggers = 'hover';
+		}
 
-	createForm() {
+/* 	createForm() {
     this.searchTheWebForm = this.formBuilder.group( {
       searchTerm: [ '', [Validators.required ] ]
     } )
-  }
+  } */
 
-	get searchTerm() {
+/* 	get searchTerm() {
     return this.searchTheWebForm.get('searchTerm');
-  }
+  } */
 
 	ngOnInit() {
-		this.createForm()
+		/* this.createForm() */
     switch (this.translateService.currentLang) {
       case 'cat':
         this.currentLang = 'ca-ES'
