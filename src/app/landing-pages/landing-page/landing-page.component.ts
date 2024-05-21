6a427ffc9842b862a40fb9369e88fe49d6771de0
ignaciollado <86431188+ignaciollado@users.noreply.github.com>
2024-05-21 08:18:49 +0200

    estoy con la home

--- src/app/landing-pages/landing-page/landing-page.component.ts
+++ src/app/landing-pages/landing-page/landing-page.component.ts
@@ -14,7 +14,7 @@ export class LandingPageComponent {
   public theLeftLema: string = "<h2><strong>CTA</strong></h2>"
 
   @Input({ required: true }) landingMainTitle: string = "Título del proyecto";
-  @Input({ required: true }) landingSlogan: string = "\"Slogan o una prase que represente la esencia del proyecto\"";
+  @Input({ required: true }) landingSlogan: string = "\"ibemprėn, recursos para emprender un negocio en las Islas Baleares.\"";
   @Input({ required: true }) landingDescription: string = "<h1>Descripción</h1><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>";
   @Input({ required: true }) landingContactData!: string;
 }
