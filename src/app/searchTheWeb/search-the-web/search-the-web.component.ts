65c3d6856aae18fe51b3ec9ffd935e166d6dd213
ignaciollado <86431188+ignaciollado@users.noreply.github.com>
2024-09-27 17:36:58 +0200

    Add files via upload

--- src/app/searchTheWeb/search-the-web/search-the-web.component.ts
+++ src/app/searchTheWeb/search-the-web/search-the-web.component.ts
@@ -25,7 +25,6 @@ export class SearchTheWebComponent {
 	pauseOnIndicator: boolean = false
 	pauseOnHover: boolean = true
 	pauseOnFocus: boolean = true
-  value = 'Clear me';
   
   @Input() showLinks: string;
 
